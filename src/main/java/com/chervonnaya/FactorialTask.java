ecb06f502bed1780a1c16cc2932502d9c6627307
lich <elizaveta.chervonnaya@gmail.com>
2024-07-18 12:02:56 +0300

    add FactorialTask and ForkJoinPoolExample classes

---
+++ src/main/java/com/chervonnaya/FactorialTask.java
@@ -0,0 +1,24 @@
+package com.chervonnaya;
+
+import java.util.concurrent.RecursiveTask;
+
+public class FactorialTask extends RecursiveTask<Long> {
+
+    private final int n;
+
+    public FactorialTask(int n) {
+        this.n = n;
+    }
+
+    @Override
+    protected Long compute() {
+        if (n == 1) {
+            return 1L;
+        } else {
+            FactorialTask subTask = new FactorialTask(n - 1);
+            subTask.fork();
+            return n * subTask.join();
+        }
+    }
+}
+
